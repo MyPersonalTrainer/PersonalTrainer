3aa28dffa1556a1bd145ae41826bbd0930b137ab
STASILu4 <stasilu4@gmail.com>
2015-10-30 21:10:21 +0200

    has_and_belongs added

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -2,4 +2,5 @@ class TrainingDay < ActiveRecord::Base
   belongs_to :muscle_group
   has_many :training_day_exercises
   has_many :exercises, :through => :training_day_exercises
+  has_and_belongs_to_many :programs
 end
