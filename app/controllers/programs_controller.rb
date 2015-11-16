8d30e07ea6d7d14ca07b53948e69b8c1dfbab99f
STASILu4 <stasilu4@gmail.com>
2015-11-16 19:23:25 +0200

    programs index

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,21 +1,16 @@
 class ProgramsController < ApplicationController
   def index
-
+    programs = []
+    Program.all.each do |p|
+      @program = p
+      programs << show_program
+    end
+    render json:  programs
   end
 
   def show
     @program = Program.find(params[:id])
-    training_plan = {}
-    @program.training_days.each do |d|
-       ex = []
-       d.exercises.each do |e|
-         ex << {e.name => e.description}
-       end
-       groups = []
-       d.muscles_groups.each {|g| groups << MuscleGroup.find(g).name}
-       training_plan[Date::DAYNAMES[d.wday]] = {groups => ex}
-    end
-    render json: training_plan
+    render json: show_program
   end
 
   def new
@@ -40,5 +35,19 @@ class ProgramsController < ApplicationController
   def program_params
     params.require(:program).permit(:gender, :age, :weight, :height, :activity, :physical_level,
     :complexity, :training_type)
+  end
+
+  def show_program
+    training_plan = {}
+    @program.training_days.each do |d|
+      ex = []
+      d.exercises.each do |e|
+        ex << {e.name => e.description}
+      end
+      groups = []
+      d.muscles_groups.each {|g| groups << MuscleGroup.find(g).name}
+      training_plan[Date::DAYNAMES[d.wday]] = {groups => ex}
+    end
+    training_plan
   end
 end
