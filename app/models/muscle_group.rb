870c540ebe31354d4eecf23796035486b6a89413
Siyko <siy2009@ukr.net>
2015-12-02 23:19:15 +0200

    only url

--- app/models/muscle_group.rb
+++ app/models/muscle_group.rb
@@ -1,6 +1,6 @@
 class MuscleGroup < ActiveRecord::Base
   has_many :exercises
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   def image_url
     image.url
